2f070be6b943f66900ff6844100c9504f64b3cbd
niel <93116556+nielxxx@users.noreply.github.com>
2023-11-08 23:32:36 +0800

    added

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              "images/splash.png", 
+              "images/groceryB1.png", 
               height: 300
             ),
             Container(
