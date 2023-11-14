90378d840fbdea70f6d6370983f2901abe1b52c1
niel <93116556+nielxxx@users.noreply.github.com>
2023-11-14 20:01:06 +0800

    asdasd

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,8 +1,83 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges; // Import the badges package with an alias
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF00A368),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(right: 20, left: 15, top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00A368),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.5),
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: badges.Badge( // Use the alias here
+                        badgeColor: Colors.red,
+                        padding: EdgeInsets.all(7),
+                        badgeContent: Text(
+                          "5",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        position: badges.BadgePosition.topEnd(top: -10, end: -10),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            CupertinoIcons.cart,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome", 
+                      style: TextStyle(
+                        fontSize: 35, 
+                        color: Colors.white, 
+                        fontWeight: 
+                        FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
